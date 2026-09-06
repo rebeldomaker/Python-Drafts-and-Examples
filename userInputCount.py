f7d80ae2c0ffc4381686439160679f92cca1d535
Reb <rebeldomaker@icloud.com>
2026-09-06 21:19:19 +0200

    new example py file

---
+++ userInputCount.py
@@ -0,0 +1,7 @@
+# Ask the user to enter a number between 1 and 20. 
+x = int(input("Enter a number between 1-20: "))
+# todo Count down from the user's number to 0 and display the countdown.
+while x > 0:
+    print(x)
+    x = x - 1
+    
