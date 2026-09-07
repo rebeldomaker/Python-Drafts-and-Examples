d831b1ff291bb7d73106a057f6e5e12ec95d3809
Reb <rebeldomaker@icloud.com>
2026-09-07 10:24:42 +0200

    ny opgaver

---
+++ Opgaver.py
@@ -0,0 +1,61 @@
+# Print the sum of the numbers from 1 to 100. (1 + 2 + 3 + ... + 100 = 5050)
+"""int sum = 0;
+foreach in (sum)
+{
+    Console.WriteLine(sum);
+sum = sum - 1;
+}"""
+"""while sum < 100:
+    sum = sum + sum
+for i in range(100):"""
+sum = 0
+for i in range(100):
+    i = 1
+    # i <= 100
+    print(i + 1)
+    
+         # todo Create a program that spells a word backwards._
+
+         # While loop
+         # todo Create a while loop that prints "Hello World!" 6 times.
+          
+         # todo Make a while loop that prints the numbers from 0 to 12, in the console.
+         # todo Make a program that asks the user to type "hello", every time the user does not type "hello", the program should print the user's words and ask the user to type "hello". When the user types "hello", the program stops.
+         # todo Make a program that asks the user for a password. If the user types "secret", the program writes "Access granted", otherwise it asks again.
+         # todo Make a program that asks the user for a number. If the number is less than 0, the program asks again. When the user enters a positive number, the program stops.
+         # If-else statement
+         # todo Make a program where you enter an age, and it prints:
+         # "You are a teenager", if the age is between 13 and 19.
+         # "You are an adult", if over 19.
+         # "You are a child", if under 13
+         # todo Make a program where the user enters a number. If the number is divisible by 2, say "Even number", otherwise "Odd numbers".
+          
+         # todo Make a simple quiz question: "What is the capital of Denmark?" If the answer is "Copenhagen", then print "Correct!", otherwise "Incorrect!".
+          
+         # todo Make a program that asks about the temperature (number). If it is below 0, print "It is freezing", if between 0 and 20, print "It is cold", otherwise "It is hot".
+          
+         # todo Make a program that checks whether a letter is a vowel or a consonant.
+          
+         # todo Make a program that finds the largest of 3 numbers.
+          
+         # Extra tasks
+         # todo Mini-login system: The user must enter a username and password. Check if it matches the correct username and password, and grant access.
+          
+         # todo Calculator: The user selects an operation (e.g. +, -, *, /) and enters two numbers. The program displays the result.
+          
+         # todo Guess a number: The computer chooses a random number between 1 and 10. The user must guess it. The program tells you if it is too high, low or correct.
+          
+         # todo Check if a number is even or odd.
+          
+         # Array
+         # todo Create an array that contains 4 of your classmates' names.
+         
+         # todo Print the length of the array.
+          
+         # todo Print the third name from the array in task 1.
+          
+         # todo Print the entire array using Foreach()
+          
+         # todo Create a 2d array with {1,2,3,4} and {5,6,7,8}
+          
+         # todo Print different places from task 5
