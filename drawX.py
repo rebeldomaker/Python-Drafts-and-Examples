ed0dbd93fccdbbe63241888155d343ee85a51f9b
Reb <rebeldomaker@icloud.com>
2026-09-07 09:45:29 +0200

    //

---
+++ drawX.py
@@ -0,0 +1,20 @@
+# todo reread code below and re-create in python
+"""int size = 5;
+
+for (int i = 0; i < size; i++)
+    {
+for (int j = 0; j < size; j++)
+{
+// Print asterisk if we're on a diagonal
+if (j == i || j == size - 1 - i)
+{
+Console.Write("*");
+}
+else
+{
+Console.Write(" ");
+}
+}
+Console.WriteLine();
+}"""
+         
